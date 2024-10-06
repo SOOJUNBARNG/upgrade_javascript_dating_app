7da21572399d4ca4ce361d20160820f2f7067f27
barng <powwerer@gmail.com>
2024-10-06 23:57:37 +0900

    new

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json()); // this ensures req.body is populated with parsed data
 app.use('/api/auth', authRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 

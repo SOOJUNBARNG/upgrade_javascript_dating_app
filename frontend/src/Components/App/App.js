f4f3163d697284232d3b230aaf2662b330214f3c
ban.sujun <ban.sujun@medical-frontier.jp>
2024-10-21 17:54:27 +0900

    new

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -3,24 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from '../Login/Login';  // Import Login component from its directory
 import Home from '../Home/Home';    // Import Home component from its directory
+import logo_img from '../../Asset/logo/logo_img.svg'; // Import your logo image
+import logo_txt from '../../Asset/logo/logo_text.svg'; // Import your logo image
 import './App.css';  // Component-specific styles
-
-// Check
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>App is working!</h1>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <div className="logo-container">
+            <img src={logo_img} alt="Logo_img" className="logo_img" />
+            <img src={logo_txt} alt="Logo_txt" className="logo_txt" />
+          </div>
           <nav>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
